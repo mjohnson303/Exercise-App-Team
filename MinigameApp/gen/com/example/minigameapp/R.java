10105dccba838250adf22a195ddeee7f58761476
Matt <mbaseball1@gmail.com>
2012-10-03 18:43:50 -0400

    Made huge directory changes. Also added a computer car to the image. Still having an error that I do not know how to fix.

--- MinigameApp/gen/com/example/minigameapp/R.java
+++ MinigameApp/gen/com/example/minigameapp/R.java
@@ -17,13 +17,16 @@ public final class R {
     }
     public static final class id {
         public static final int button_scan=0x7f070006;
-        public static final int discoverable=0x7f07000b;
+        public static final int discoverable=0x7f07000e;
         public static final int in=0x7f070007;
-        public static final int insecure_connect_scan=0x7f07000a;
-        public static final int menu_settings=0x7f070008;
+        public static final int insecure_connect_scan=0x7f07000d;
+        public static final int mTitle=0x7f07000a;
+        public static final int menu_settings=0x7f07000b;
         public static final int new_devices=0x7f070005;
         public static final int paired_devices=0x7f070003;
-        public static final int secure_connect_scan=0x7f070009;
+        public static final int secure_connect_scan=0x7f07000c;
+        public static final int start=0x7f070009;
+        public static final int title=0x7f070008;
         public static final int title_left_text=0x7f070000;
         public static final int title_new_devices=0x7f070004;
         public static final int title_paired_devices=0x7f070002;
@@ -46,9 +49,10 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int bt_not_enabled_leaving=0x7f040003;
         public static final int button_scan=0x7f04000f;
-        public static final int discoverable=0x7f040013;
+        public static final int discoverable=0x7f040014;
         public static final int hello_world=0x7f040007;
-        public static final int insecure_connect=0x7f040012;
+        public static final int insecure_connect=0x7f040013;
+        public static final int main_page_title=0x7f040011;
         public static final int menu_settings=0x7f040008;
         public static final int none_found=0x7f04000c;
         public static final int none_paired=0x7f04000b;
@@ -58,7 +62,7 @@ public final class R {
         public static final int scanning=0x7f040009;
         /**  Options Menu 
          */
-        public static final int secure_connect=0x7f040011;
+        public static final int secure_connect=0x7f040012;
         public static final int select_device=0x7f04000a;
         /**   BluetoothChat 
          */
--- MinigameApp/src/com/example/minigameapp/GameLayer.java
+++ MinigameApp/src/com/example/minigameapp/GameLayer.java
@@ -13,10 +13,12 @@ import org.cocos2d.types.CGPoint;
 import org.cocos2d.types.CGSize;
 import org.cocos2d.types.ccColor4B;
 
+
 import android.util.Log;
 
 public class GameLayer extends CCColorLayer{
 	CCSprite player;
+	CCSprite computer;
 	public static CCScene scene()
 	{
 	    CCScene scene = CCScene.node();
@@ -30,10 +32,14 @@ public class GameLayer extends CCColorLayer{
 	protected GameLayer(ccColor4B color)
 	{
 	    super(color);
+	    this.setIsTouchEnabled(true);
 	    CGSize winSize = CCDirector.sharedDirector().displaySize();
-	    player = CCSprite.sprite("race_car.jpg");
-	 
-	    player.setPosition(CGPoint.ccp(player.getContentSize().width / 2.0f, winSize.height / 2.0f));
+	    
+	    player = CCSprite.sprite("race_car.png");
+	    computer = CCSprite.sprite("race_car.png");
+	    player.setPosition(CGPoint.ccp(player.getContentSize().width / 2.0f,player.getContentSize().height/2.0f));
+	    computer.setPosition(CGPoint.ccp(computer.getContentSize().width / 2.0f+player.getContentSize().width,computer.getContentSize().height/2.0f + player.getContentSize().height));
+
 	 
 	    addChild(player);
 	}
@@ -49,7 +55,7 @@ public class GameLayer extends CCColorLayer{
 	    int actualDuration = rand.nextInt(rangeDuration) + minDuration;
 	    
 	    Log.d("GameLayer","Set Action");
-		CCMoveTo actionMove = CCMoveTo.action(actualDuration, CGPoint.ccp(-player.getContentSize().width / 2.0f, finalX));
+		CCMoveTo actionMove = CCMoveTo.action(actualDuration, CGPoint.ccp(-computer.getContentSize().width / 2.0f, finalX));
 	    CCCallFuncN actionMoveDone = CCCallFuncN.action(this, "spriteMoveFinished");
 	    CCSequence actions = CCSequence.actions(actionMove, actionMoveDone);
 	    Log.d("GameLayer", "Start Moving");
@@ -60,7 +66,7 @@ public class GameLayer extends CCColorLayer{
 		Log.d("GameLayer", "Finished Moving");
 	    //CCSprite sprite = (CCSprite)sender;
 	    //this.removeChild(sprite, true);
-	    this.removeChild(player, true);
+	    this.removeChild(computer, true);
 		Log.d("GameLayer", "Remove Sprite");
 	}
 }
